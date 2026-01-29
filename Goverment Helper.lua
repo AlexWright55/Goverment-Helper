c8333334b40028bf6288f916d284d90ce2ef4bd0
AlexWright55 <AlexandrAbramenko003@yandex.ru>
2026-01-30 03:26:34 +0600

    v0.0.0.1 Фикс ссылки на автообновление

--- Goverment Helper.lua
+++ Goverment Helper.lua
@@ -2806,7 +2806,7 @@ function check_update()
 	local path = configDirectory .. "/Update_Info.json"
 	os.remove(path)
 	local url =
-	'https://raw.githubusercontent.com/Alexandr-Botovod/Prison_Helper/refs/heads/main/PrisonHelper/Update_info.json'
+	'https://alexwright55.github.io/Goverment-Helper/Goverment%20Helper/Update_Info.json'
 	if isMonetLoader() then
 		downloadToFile(url, path, function(type, pos, total_size)
 			if type == "finished" then
